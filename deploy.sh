65562f84ea315b56fa49fd9130c2e086c77568f2
Peter Tran <peter.tran@msionline.com>
2019-12-18 09:24:59 -0600

    changed git push repository

--- deploy.sh
+++ deploy.sh
@@ -20,4 +20,4 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f https://github.com/petertranfl/leagueapp.git master:gh-pages
